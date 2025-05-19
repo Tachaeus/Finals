7341b50daac23311b435221c39d3586fb48b60d1
Abdallah Abdul <tachincursion@gmail.com>
2025-05-19 19:12:48 +0300

    Update card.jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -29,7 +29,7 @@ function Card() {
                         <p>Our work may not be the best you've ever seen,<br /> <span>but rest assured,</span><br /> it is what's just right for you!</p>
                         <a href=""><button>Take a Look</button></a>
                     </div>
-                    <img className="desktop_img" src="https://i.ibb.co/Tsz01kv/removed6.png" />
+                    <img className="desktop_img" src="https://i.ibb.co/QjqqzR1m/removed6-2.png" />
                 </div><br /><br />
                 <div className="find">
                     <div id="find">
