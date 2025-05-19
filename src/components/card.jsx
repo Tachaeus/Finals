73e95ee5e7f3ec68c8467bd988bd1279177d9493
Abdallah Abdul <tachincursion@gmail.com>
2025-05-19 18:40:33 +0300

    Update card.jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -23,7 +23,7 @@ function Card() {
             <main>
                 <section className="section">
                 <div className="start">
-                    <img className="mobile_img" src="https://i.ibb.co/zV8096KR/less.png" />
+                    <img className="https://i.ibb.co/dsL6WJ0z/removed5.png" />
                     <div className="in_image_text">
                         <p>Masterful Coding Solutions <br /> Optimizing Potential</p>
                         <p>Our work may not be the best you've ever seen,<br /> <span>but rest assured,</span><br /> it is what's just right for you!</p>
